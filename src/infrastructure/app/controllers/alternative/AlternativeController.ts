95619cff2b14a166043b981e68c50ce611e62b7a
Jorge Luis Guiza <jorge.guiza@silverapp.co>
2023-05-17 16:36:27 -0500

    🪡 Chore: Change on controllers files of geoip and add new libraris. 🪡

--- src/infrastructure/app/controllers/alternative/AlternativeController.ts
+++ src/infrastructure/app/controllers/alternative/AlternativeController.ts
@@ -1,13 +1,18 @@
 import { type Request, type Response } from 'express'
 import geoip from 'geoip-lite'
+import requestIp from 'request-ip'
 
 export default class AlternativeController {
   async myGeo(req: Request, res: Response): Promise<Response> {
-    const myIP = req.ip
+    const myIP = req.clientIp as string
+    const normalIp = req.ip
+    const clientIp = requestIp.getClientIp(req) as string
     const ifonIP = geoip.lookup(myIP)
     return res.status(200).json({
       ok: true,
-      myIP,
+      myIP: `My ip ${myIP}`,
+      clientIp: `Client IP ${clientIp}`,
+      normalIp,
       ifonIP
     })
   }
